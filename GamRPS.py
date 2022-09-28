09ce7878b7ee3d745269a1b7f896ad3ef3d07545
Purvciems <114050670+Purvciems@users.noreply.github.com>
2022-09-28 08:19:29 +0300

    Update GamRPS.py

--- GamRPS.py
+++ GamRPS.py
@@ -1,15 +1,24 @@
-hooman_t = input('Input Hooman turn: ')
-computer_t = 'scissors'
+import random
+turn = ['rock', 'paper', 'scissors']
 
-if hooman_t == computer_t:
-    print('Draw?')
-elif hooman_t == 'rock' and computer_t == 'scissors':
-    print('Human wins lol')
-
-elif hooman_t == 'paper' and computer_t == 'rock':
-    print('Human wins lol')
+while True:
+    hooman_t = input('Input Hooman turn or exit: ')
+    if hooman_t == 'exit':
+        print('ok goodbye')
+        break
+    computer_t = random.choice(turn)
+    print(f'Human: {hooman_t} vs Computer: {computer_t}')
     
-elif hooman_t == 'scissors' and computer_t == 'paper':
-    print('Human wins lol')
-else:
-    ('PC WINS HAHAHA')
+
+    if hooman_t == computer_t:
+        print('Draw?')
+    elif hooman_t == 'rock' and computer_t == 'scissors':
+        print('Human wins lol')
+
+    elif hooman_t == 'paper' and computer_t == 'rock':
+        print('Human wins lol')
+    
+    elif hooman_t == 'scissors' and computer_t == 'paper':
+        print('Human wins lol')
+    else:
+        print('PC WINS HAHAHA')
